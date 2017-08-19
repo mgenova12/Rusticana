b9dd5a4fbdd9f8cedfde1fa974f45421897f68b5
Marco <mgenova03@gmail.com>
2017-08-19 11:46:45 -0400

    add employee login

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,8 @@
+class AdminsController < ApplicationController
+  
+  def index 
+
+    render 'index.html.erb'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
   get '/about' => 'home#about'
   get '/gallery' => 'home#gallery'
 
+  # get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/employee_login" => "sessions#new"
+  post "/employee_login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  get "admin_panel" => 'admins#index'
+
   # resources :photos, only: [:new, :create, :index, :destroy]
 end
