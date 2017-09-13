861a33c9bbe69b464ddec44605d24226cdb541d6
Marco <mgenova03@gmail.com>
2017-09-13 11:31:58 -0400

    email sent with params and link

--- app/helpers/apply_helper.rb
+++ app/helpers/apply_helper.rb
@@ -4,11 +4,19 @@ module ApplyHelper
     mg_client = Mailgun::Client.new(ENV["MAILGUN_API_KEY"])
 
     message_params =  {
-                       from: 'mgenova03@gmail.com',
-                       to:   'mgenova03@gmail.com',
-                       subject: "Rusticana Application",
-                       text:    "Is this working?"
-                      }
+      from: 'mgenova03@gmail.com',
+      to:   'mgenova03@gmail.com',
+      subject: "Rusticana Application",
+      html:  "
+      <h3> You have a new application from #{@apply[:first_name]} #{@apply[:last_name]} for the position of #{@apply[:position]}.</h3>
+        
+        <a href='http://localhost:3000/admin_panel'><button>View Application</button></a>
+      "
+
+
+
+      }
+                      
 
     result = mg_client.send_message(ENV["MAILGUN_DOMAIN"], message_params).to_h!
 
--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,4 +1,4 @@
 class Apply < ApplicationRecord
-  validates :first_name, :last_name, :address, :city, :state, :zip, :phone_number, :date_of_birth, :eighteen_or_older, :prevented, presence: true
+  # validates :first_name, :last_name, :address, :city, :state, :zip, :phone_number, :date_of_birth, :eighteen_or_older, :prevented, presence: true
 
 end
