796a8ee13283b65ca5576d15b025d276b8299582
Marco <mgenova03@gmail.com>
2018-01-10 16:03:50 -0500

    rename admin routes

--- app/controllers/admin/job_applications_controller.rb
+++ app/controllers/admin/job_applications_controller.rb
@@ -1,16 +1,12 @@
-class AdminsController < ApplicationController
+class Admin::JobApplicationsController < ApplicationController
   before_action :authenticate_user!
   layout "admin"
 
   def index 
-    @specials = Special.all
-  end
-
-  def applications 
     @applications = Apply.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
   end
 
-  def applications_show 
+  def show 
     @application = Apply.find(params[:id])
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/admin_panel'
+      redirect_to '/admin/applications'
     else
       flash[:danger] = 'Invalid email or password!'
       redirect_to '/employee_login'
