5335253fadc64f465b0b654b6183634a669d31ed
Marco <mgenova03@gmail.com>
2018-04-15 17:32:34 -0400

    add new email

--- app/helpers/apply_helper.rb
+++ app/helpers/apply_helper.rb
@@ -5,14 +5,14 @@ module ApplyHelper
 
     message_params =  {
       from: 'rusticanamail@gmail.com',
-      to:   'mgenova03@gmail.com, jgenova80@hotmail.com, genova22@gmail.com',
+      to:   'mgenova03@gmail.com, jgenova80@hotmail.com, rusticanadoverrd@gmail.com',
       subject: "Rusticana Application",
       html:  "
       <p> You have a new application from <b>#{@apply[:first_name]} #{@apply[:last_name]} </b> for the position of <b>#{@apply[:position]}</b>.<p>
         
         <a href='http://www.rusticanapizza.com/admin/applications'><button>View Application</button></a>
       ",
-      :'recipient-variables' => '{"mgenova03@gmail.com": {"first":"Marco", "id":1}, "jgenova80@hotmail.com": {"first":"Joann", "id": 2}, "genova22@gmail.com": {"first":"Anthony", "id": 3}}'
+      :'recipient-variables' => '{"mgenova03@gmail.com": {"first":"Marco", "id":1}, "jgenova80@hotmail.com": {"first":"Joann", "id": 2}, "rusticanadoverrd@gmail.com": {"first":"Anthony", "id": 3}}'
       }
                       
 
